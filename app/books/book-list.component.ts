c4f4c973ac6535f994f3db81b039cceda117b048
tbaehr23 <tbaehr@solnwerks.com>
2016-09-11 06:48:18 -0500

    Move static data from component to separate mock data file
    create BOOKSMOCK constant that returns Book[]in a books-mock.ts file
    
    move static data from BookListComponent books property into BOOKSMOCK
    
    import BOOKSMOCK into BookListComponent and initialize books property
    with BOOKSMOCK

--- app/books/book-list.component.ts
+++ app/books/book-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { Book } from './book'; //relative to this file
+import { BOOKSMOCK } from './books-mock';
 
 @Component({
     selector: 'book-list',
@@ -9,10 +10,5 @@ import { Book } from './book'; //relative to this file
 export class BookListComponent {
     
     pageTitle: string = "Book List";
-    books: Book[] = [
-        {id: 1, title: 'The Old Man and the Sea', authorName: 'Ernest Hemingway', genre: 'fiction', year:'1951', price: 25.00 },
-        {id: 2, title: 'The Catcher in the Rye', authorName: 'J. D. Salinger', genre: 'fiction', year:'1951', price: 20.00},
-        {id: 3, title: 'Macbeth', authorName: 'William Shakespeare', genre: 'tragedy', year:'1623', price: 15.95},
-        {id: 4, title: 'Profiles in Courage', authorName: 'John F. Kennedy', genre: 'biography', year:'1955', price:35.95}
-    ];
+    books: Book[] = BOOKSMOCK;
 }
---
+++ app/books/books-mock.ts
@@ -0,0 +1,6 @@
+export const BOOKSMOCK  = [
+        {id: 1, title: 'The Old Man and the Sea', authorName: 'Ernest Hemingway', genre: 'fiction', year:'1951', price: 25.00 },
+        {id: 2, title: 'The Catcher in the Rye', authorName: 'J. D. Salinger', genre: 'fiction', year:'1951', price: 20.00},
+        {id: 3, title: 'Macbeth', authorName: 'William Shakespeare', genre: 'tragedy', year:'1623', price: 15.95},
+        {id: 4, title: 'Profiles in Courage', authorName: 'John F. Kennedy', genre: 'biography', year:'1955', price:35.95}
+    ];
