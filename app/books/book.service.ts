1369a6708e208982135cc718e1b608e627d6163d
tbaehr23 <tbaehr@solnwerks.com>
2016-09-11 07:24:33 -0500

    Add strong typing to BookService

--- app/books/book.service.ts
+++ app/books/book.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 
+import { Book } from './book';
 import { BOOKSMOCK } from './books-mock';
 
 @Injectable()
 export class BookService {
 
-    getBooks(){
+    getBooks(): Book[]{
         return BOOKSMOCK;
     }
 }
